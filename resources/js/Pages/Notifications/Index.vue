9c06df68c13d23b123e22cba4636161693705ed7
milchevyordan <yordan@jamesoutsourcing.com>
2024-11-04 21:50:28 +0200

    Add dark mode to select

--- resources/js/Pages/Notifications/Index.vue
+++ resources/js/Pages/Notifications/Index.vue
@@ -58,7 +58,7 @@ const markRead = (itemId: number) => {
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+                    class="bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <Table
--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -46,7 +46,7 @@ const save = async (only?: Array<string>) => {
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+                    class="bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <div class="grid lg:grid-cols-1 xl:grid-cols-2 gap-4">

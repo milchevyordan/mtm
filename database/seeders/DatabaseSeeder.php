238f63f93f09f637db512c8f37584deb41cc6603
milchevyordan <milchevyordan2@gmail.com>
2024-11-28 16:40:28 +0200

    Comment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,76 +29,76 @@ class DatabaseSeeder extends Seeder
             'warehouse' => Warehouse::Varna->value,
         ]);
 
-        $productNames = [
-            'MAXI 1500 anti skid flooring',
-            'MAXI 5000 anti slip flooring',
-            'SL 3000',
-            'TOPP 4000',
-            'STONE CARPET',
-            'Flowcoat SF 41 - epoxy flooring',
-            'ANTI STATIC FLOORING',
-            'Peran SL self leveling flooring',
-            'FAST-CLAD 7220',
-            'Macropoxy L425',
-            'Macropoxy K267',
-            'Diamond cutting blades for granite stone',
-            'POLISHING PADS',
-            'Drilling core bits - Wet',
-            'Resuthane™ SL',
-            'Corrosion and chemical protective coatings and paints',
-            'Fire protective coatings and paints',
-            'Vespupark',
-            'PU- coating for outdoor areas',
-            'LITHURIN Hard',
-            'STEEL FIBERS',
-            'POLISHED CONCRETE FLOORINGS',
-            'Diamond grinding wheels',
-            'Marine engine spares',
-        ];
-
-        $productInserts = [];
-        $productQuantityInserts = [];
-        foreach ($productNames as $key => $productName) {
-            $productInserts[] = [
-                'creator_id'       => 1,
-                'internal_id'      => $faker->numerify('#####'),
-                'name'             => $productName,
-                'minimum_quantity' => 10,
-                'created_at'       => now(),
-            ];
-
-            $productQuantityInserts[] = [
-                'product_id' => $key + 1,
-                'warehouse'  => Warehouse::Varna->value,
-                'quantity'   => 10000,
-            ];
-
-            $productQuantityInserts[] = [
-                'product_id' => $key + 1,
-                'warehouse'  => Warehouse::France->value,
-                'quantity'   => 10000,
-            ];
-
-            $productQuantityInserts[] = [
-                'product_id' => $key + 1,
-                'warehouse'  => Warehouse::Netherlands->value,
-                'quantity'   => 10000,
-            ];
-        }
-
-        Product::insert($productInserts);
-        ProductQuantity::insert($productQuantityInserts);
-
-        $projectInserts = [];
-        for ($i = 0; $i < 3; $i++) {
-            $projectInserts[] = [
-                'creator_id' => 1,
-                'warehouse'  => $faker->randomElement(Warehouse::values()),
-                'name'       => $faker->city(),
-                'created_at' => now(),
-            ];
-        }
-
-        Project::insert($projectInserts);
+        //        $productNames = [
+        //            'MAXI 1500 anti skid flooring',
+        //            'MAXI 5000 anti slip flooring',
+        //            'SL 3000',
+        //            'TOPP 4000',
+        //            'STONE CARPET',
+        //            'Flowcoat SF 41 - epoxy flooring',
+        //            'ANTI STATIC FLOORING',
+        //            'Peran SL self leveling flooring',
+        //            'FAST-CLAD 7220',
+        //            'Macropoxy L425',
+        //            'Macropoxy K267',
+        //            'Diamond cutting blades for granite stone',
+        //            'POLISHING PADS',
+        //            'Drilling core bits - Wet',
+        //            'Resuthane™ SL',
+        //            'Corrosion and chemical protective coatings and paints',
+        //            'Fire protective coatings and paints',
+        //            'Vespupark',
+        //            'PU- coating for outdoor areas',
+        //            'LITHURIN Hard',
+        //            'STEEL FIBERS',
+        //            'POLISHED CONCRETE FLOORINGS',
+        //            'Diamond grinding wheels',
+        //            'Marine engine spares',
+        //        ];
+        //
+        //        $productInserts = [];
+        //        $productQuantityInserts = [];
+        //        foreach ($productNames as $key => $productName) {
+        //            $productInserts[] = [
+        //                'creator_id'       => 1,
+        //                'internal_id'      => $faker->numerify('#####'),
+        //                'name'             => $productName,
+        //                'minimum_quantity' => 10,
+        //                'created_at'       => now(),
+        //            ];
+        //
+        //            $productQuantityInserts[] = [
+        //                'product_id' => $key + 1,
+        //                'warehouse'  => Warehouse::Varna->value,
+        //                'quantity'   => 10000,
+        //            ];
+        //
+        //            $productQuantityInserts[] = [
+        //                'product_id' => $key + 1,
+        //                'warehouse'  => Warehouse::France->value,
+        //                'quantity'   => 10000,
+        //            ];
+        //
+        //            $productQuantityInserts[] = [
+        //                'product_id' => $key + 1,
+        //                'warehouse'  => Warehouse::Netherlands->value,
+        //                'quantity'   => 10000,
+        //            ];
+        //        }
+        //
+        //        Product::insert($productInserts);
+        //        ProductQuantity::insert($productQuantityInserts);
+        //
+        //        $projectInserts = [];
+        //        for ($i = 0; $i < 3; $i++) {
+        //            $projectInserts[] = [
+        //                'creator_id' => 1,
+        //                'warehouse'  => $faker->randomElement(Warehouse::values()),
+        //                'name'       => $faker->city(),
+        //                'created_at' => now(),
+        //            ];
+        //        }
+        //
+        //        Project::insert($projectInserts);
     }
 }
